090b14539e666ba6652bc8cb895e16dd4d2aa804
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-06-09 11:19:39 -0400

    organized file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Navbar from "./Navbar";
-// import jobs from "./data/jobs.json"
+import Nav from './shared/Nav';
+
 import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./Home"
-import Jobs from "./Jobs";
-import Companies from "./Companies";
+import Jobs from "./jobs/Jobs";
+import Companies from "./companies/Companies";
 
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-      <Navbar/>
+      <Nav/>
       
       <Routes>
         <Route path="/" element={<Home />} />
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import Search from "./Search";
-import JobList from "./JobList";
+import JobList from "./jobs/JobList";
 // import jobs from "./data/jobs.json"
 import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
