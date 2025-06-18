b4eecd12114005e70199ac52c8c456950ec74404
Moultrie Dangerfield <moultriedanger@gmail.com>
2025-06-18 10:49:45 -0400

    created link to company page on jobs

--- src/jobs/Job.js
+++ src/jobs/Job.js
@@ -1,11 +1,38 @@
-import { useParams} from "react-router-dom"
+import { createSearchParams, useAsyncError, useParams} from "react-router-dom"
 import useJobData from "./useJobData"
+import { Link } from "react-router-dom"
+import {useState, useEffect} from "react"
 
 const Job = () => {
+
     const {id} = useParams()
 
     const jobData = useJobData(id)
+    
+    const [companyId, setCompanyId] = useState(null)
 
+    useEffect(() =>{
+        const fetchCompany = async () => {
+
+            const url = `http://localhost:8080/jobs/${id}/company`
+
+            try{
+                const companyResponse = await fetch(url);
+                
+                const companyData = await companyResponse.json();
+
+                const cId = companyData.companyId;
+                console.log(companyData);
+
+                setCompanyId(cId);
+            }
+            catch (error){
+                console.error("Caught an error:", error.message);
+            }
+        }
+        fetchCompany();
+    },[])
+    
     if (!jobData) {
     return <p>Loading job data...</p>;
     }
@@ -14,6 +41,11 @@ const Job = () => {
         <>
         <h1>{jobData.jobTitle}</h1>
         <h2>{jobData.companyName}</h2>
+
+        <Link to = {`/companies/${companyId}/jobs`}>
+            <button>Company Page</button>
+        </Link>
+        
         <h2>Full Job Description:</h2>
         <p>{jobData.jobDescription}</p>
         </>
